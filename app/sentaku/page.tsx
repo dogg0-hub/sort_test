b37a78e24a72c8574a91705adc88aef271fe54bb
dogg0-hub <konnichidoggo@gmail.com>
2026-08-16 09:48:13 +0900

    途中経過

--- app/sentaku/page.tsx
+++ app/sentaku/page.tsx
@@ -2,17 +2,33 @@
 import { useState } from "react";
 
 export default function Home() {
-  const array = [2.909, -1000, 0.12, 0.13, 1.233, 4.233, 54, 3, 7 , 2000, 10, 9 , 8];
+  const array = [209, 1, 12, 3, 1233, 4233, 54, 3, 7 , 2000, 10, 9 , 8];
   let pointer : number[] = [];
   const [result, setResult] = useState<number[]>([]);
   let min;
+  let array_with_index : number[][] = [];
 
   function Caluculate(){
     // 最小値を取ってくる
     // 先頭と比較
-    const aryMin = function(a : number, b: number){return Math.min(a, b);}
-    min = array.reduce(aryMin);
+    array_with_index.push([0, array[0]]);
+    let min = array.reduce(function(accumulator, currentValue, currentIndex, array){
+      console.log("reduce内：currentIndexは" + currentIndex + "、currentValueは" + currentValue);
+      array_with_index.push([currentIndex, currentValue]);
+      // array_with_index.push([currentIndex, array[currentIndex]]);
+      return Math.min(accumulator, currentValue);
+    });
+     // accumulatorのcurrentIndexを入れたい
+      
+    min2 = array.filter((item, index)=>item);
+    
     console.log("min",min);
+    // ここで配列に番号がついた
+    console.log("array_with_index", array_with_index);
+    //最小値を取り出しつつ、それが配列の何番目か判断できるようにしないといけない
+    // if(array[1] >){
+
+    // }
   }
 
   return (
