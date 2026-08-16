da3a1e1c829709be5b5a3e277d4fe1a738604945
dogg0-hub <konnichidoggo@gmail.com>
2026-08-16 10:27:04 +0900

    途中経過

--- app/sentaku/page.tsx
+++ app/sentaku/page.tsx
@@ -2,33 +2,39 @@
 import { useState } from "react";
 
 export default function Home() {
-  const array = [209, 1, 12, 3, 1233, 4233, 54, 3, 7 , 2000, 10, 9 , 8];
+  const array = [209, 1, 3, 1, 1 , -1, -1, 0, 12, 3, 1233, 4233, 54, 3, 7 , 2000, 10, 9 , 8];
   let pointer : number[] = [];
   const [result, setResult] = useState<number[]>([]);
   let min;
-  let array_with_index : number[][] = [];
+  let array_with_index : number[] = [];
 
   function Caluculate(){
-    // 最小値を取ってくる
+    // 最小値を取ってくる DONE
     // 先頭と比較
-    array_with_index.push([0, array[0]]);
+    array_with_index[0]=array[0];
     let min = array.reduce(function(accumulator, currentValue, currentIndex, array){
       console.log("reduce内：currentIndexは" + currentIndex + "、currentValueは" + currentValue);
-      array_with_index.push([currentIndex, currentValue]);
-      // array_with_index.push([currentIndex, array[currentIndex]]);
+      array_with_index.push(array[currentIndex]);
       return Math.min(accumulator, currentValue);
     });
-     // accumulatorのcurrentIndexを入れたい
-      
-    min2 = array.filter((item, index)=>item);
-    
+
     console.log("min",min);
     // ここで配列に番号がついた
     console.log("array_with_index", array_with_index);
-    //最小値を取り出しつつ、それが配列の何番目か判断できるようにしないといけない
-    // if(array[1] >){
-
-    // }
+    // 番号を取り出して、最小値を取ってくる
+    let num = array_with_index.indexOf(min);
+    console.log("arrayのminは" + array_with_index.indexOf(min) + "中身は" + array_with_index[num]);
+    //最小値を取り出しつつ、それが配列の何番目か判断できるようにしないといけない DONE
+    console.log("やるまえ" + array_with_index);
+    console.log("やるまえ個別", array_with_index[0] + "," + array_with_index[num]);
+    for(let i = 0; i < array_with_index.length; i++){
+      
+      if(array_with_index[i] > array_with_index[num]){
+        [array_with_index[i], array_with_index[num]] = [array_with_index[num], array_with_index[i]];
+        console.log("やったあと", array_with_index[i] + "," + array_with_index[num]);
+      }
+      console.log("現時点は" + array_with_index);
+    }
   }
 
   return (
