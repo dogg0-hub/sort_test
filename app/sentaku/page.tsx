dc5efe0b438023d2856c36fed8ddf97d230b67e8
dogg0-hub <konnichidoggo@gmail.com>
2026-08-16 11:19:36 +0900

    途中経過

--- app/sentaku/page.tsx
+++ app/sentaku/page.tsx
@@ -2,9 +2,10 @@
 import { useState } from "react";
 
 export default function Home() {
-  const array = [209, 1, 3, 1, 1 , -1, -1, 0, 12, 3, 1233, 4233, 54, 3, 7 , 2000, 10, 9 , 8];
+  const array = [ 4 , 3, 2, 1, 6, 5];
   let pointer : number[] = [];
-  const [result, setResult] = useState<number[]>([]);
+  let result : number[] = [];
+  // const [result, setResult] = useState<number[]>([]);
   let min;
   let array_with_index : number[] = [];
 
@@ -12,11 +13,25 @@ export default function Home() {
     // 最小値を取ってくる DONE
     // 先頭と比較
     array_with_index[0]=array[0];
-    let min = array.reduce(function(accumulator, currentValue, currentIndex, array){
-      console.log("reduce内：currentIndexは" + currentIndex + "、currentValueは" + currentValue);
-      array_with_index.push(array[currentIndex]);
-      return Math.min(accumulator, currentValue);
-    });
+
+    for(let i = 0; i < array_with_index.length; i++){
+    
+      if(i === 0){
+        min = array.reduce(function(accumulator, currentValue, currentIndex, array){
+        console.log("reduce内：currentIndexは" + currentIndex + "、currentValueは" + currentValue);
+          array_with_index.push(array[currentIndex]); 
+        return Math.min(accumulator, currentValue);
+      });
+      }else{
+        min = result.reduce(function(accumulator, currentValue, currentIndex, array){
+        // console.log("reduce内：currentIndexは" + currentIndex + "、currentValueは" + currentValue);
+        if(i == 0){
+          array_with_index.push(array[currentIndex]); 
+        }
+        return Math.min(accumulator, currentValue);
+      });
+      }
+    
 
     console.log("min",min);
     // ここで配列に番号がついた
@@ -26,14 +41,16 @@ export default function Home() {
     console.log("arrayのminは" + array_with_index.indexOf(min) + "中身は" + array_with_index[num]);
     //最小値を取り出しつつ、それが配列の何番目か判断できるようにしないといけない DONE
     console.log("やるまえ" + array_with_index);
-    console.log("やるまえ個別", array_with_index[0] + "," + array_with_index[num]);
-    for(let i = 0; i < array_with_index.length; i++){
-      
+  
+    
       if(array_with_index[i] > array_with_index[num]){
+        console.log("やるまえ個別", array_with_index[i] + "," + array_with_index[num]);
         [array_with_index[i], array_with_index[num]] = [array_with_index[num], array_with_index[i]];
         console.log("やったあと", array_with_index[i] + "," + array_with_index[num]);
       }
       console.log("現時点は" + array_with_index);
+      result = array_with_index.slice(i + 1);
+      console.log("さいしゅう：",result);
     }
   }
 
