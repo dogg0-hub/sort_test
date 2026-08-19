7338e88373ca4635f28bbd91c76163d1cddd24cd
dogg0-hub <konnichidoggo@gmail.com>
2026-08-19 21:43:11 +0900

    完成

--- app/(prove_dont_use_ai)/page copy.tsx
+++ app/(prove_dont_use_ai)/page copy.tsx
@@ -6,18 +6,37 @@ export default function Home() {
   const array = [8,1,2,10, 1, 2, 6,4, 3, 5,7, 4, 1,9];
   let pointer : number[] = [];
   const [result, setResult] = useState<number[]>([]);
+  // 最初の1番左から、右隣を見る
 
   function Caluculate(){
     while(pointer.length !== array.length){
       for(let i = 0; i < array.length - 1; i++){
+        console.log("===");
+        console.log((i+1) + "巡目");
+        console.log("array.lenhhは" + array.length);
+        console.log("pointer.length" + pointer.length);
+        console.log("【for文】array" + i + "は" + array[i]);
+        console.log("【for文】arrayi+1" + "は" + array[i + 1]!);
         if(array[i] > array[i+1]){
+          console.log("【if文】array" + i + "は" + array[i]);
+          console.log("【if文】array")
+          console.log(i+1)
+          console.log("は" + array[i + 1]);
           [array[i], array[i+1]] = [array[i+1], array[i]];
+          console.log("【if文結果】array" + i + "は" + array[i]);
+          console.log("【if文結果】array")
+          console.log(i+1)
+          console.log("は" + array[i + 1]);
         }
+        console.log("result:" + array);
+        console.log("pointer" + pointer);
       }
       pointer.push(1);
     }
     setResult(array);
   }
+
+  //一番最後のやつは確定
 
   return (
     <>
